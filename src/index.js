f55970c88f83cd9178bfd2678ea666da179c0f73
Smit Kothari <smit.kothari2023@vitstudent.ac.in>
2024-09-20 20:15:16 +0530

    feat: Added google oauth

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="696308765074-26n2m5jnnpuo3g3ai1qpr0u0v0e7djbu.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
